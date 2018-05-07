36329770dbd2a175e404ceba0c13c8f5d0fa8539
Ilan Copelyn <ilan@netreturn.co.za>
2018-05-07 14:01:08 +0200

    2.1.2 workaround = .default

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import fetch from 'node-fetch'; // works after webpack - requires --experimental-modules & .mjs in order to work in node
-const fetch = require('node-fetch'); // broken after webpack - works in node
+const fetch = require('node-fetch').default; // broken after webpack - works in node
 
 console.log(fetch);
 
